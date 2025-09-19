a9e191f9f0e334dac5a00d00b0a3528b365a7cd2
Prajwal Kadam <prajwalkadam002@gmail.com>
2025-09-19 13:25:39 +0530

    Remove old paytm project from repository

---
+++ Paytm2/backend/db/index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const db = process.env.Mongo;
+
+mongoose.connect(db)
+    .then(() => {
+        console.log("Connected to MongoDB successfully");
+    })
+    .catch(err => {
+        console.error("Mongo Connection error: ", err);
+    });
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User,
+    Account
+}
